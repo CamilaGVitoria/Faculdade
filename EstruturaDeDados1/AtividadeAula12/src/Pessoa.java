b7dface94218f223bb45f8210bc98920f1fd0835
CamzHair <camilagvitoria1@gmail.com>
2023-05-22 10:24:43 -0300

    Atividade Aula 12

---
+++ EstruturaDeDados1/AtividadeAula12/src/Pessoa.java
@@ -0,0 +1,55 @@
+class Pessoa {
+    private int numero;
+    private String nome;
+    private String telefone;
+    private String endereco;
+    private String cpf;
+    
+    public Pessoa(int numero, String nome, String telefone, String endereco, String cpf) {
+        this.numero = numero;
+        this.nome = nome;
+        this.telefone = telefone;
+        this.endereco = endereco;
+        this.cpf = cpf;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
