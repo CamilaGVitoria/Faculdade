0578f0b6fbe1fb55136f2b25bd1bd409e3da7c4b
Camila Gonçalves Vitória <102991464+CamzHair@users.noreply.github.com>
2024-03-12 10:08:57 -0300

    Atividade Completa

---
+++ EstruturaDeDados ED1/AtividadeAula4/src/Ex351/Ex351.java
@@ -0,0 +1,31 @@
+package Ex351;
+import java.util.Scanner;
+
+public class Ex351 {
+    public static void main(String[] args) {
+        Scanner recebe = new Scanner(System.in);
+        
+        String nomes[] = new String[5];
+
+        for(int i=0; i<5; i++){
+            System.out.println("Digite um nome: ");
+            nomes[i] = recebe.nextLine();
+        }
+        
+        System.out.println("Digite o número da pessoa: ");
+        int numero = recebe.nextInt();
+
+        do{
+            System.out.println("Número inválido!");
+            
+            System.out.println("Digite o número da pessoa: ");
+            numero = recebe.nextInt();
+
+        }while (numero>5 || numero<1); 
+
+        System.out.println("O nome pertence a pessoa: " + nomes[numero - 1]);
+
+        recebe.close();
+
+    }
+}
---
+++ EstruturaDeDados ED1/AtividadeAula4/src/Ex356/Ex356.java
@@ -0,0 +1,50 @@
+package Ex356;
+import java.util.Scanner;
+
+public class Ex356 {
+    public static void main(String[] args) {
+        Scanner recebe = new Scanner(System.in);
+        
+        String nome[] = new String[15];
+        double notaPr1[] = new double[15];
+        double notaPr2[] = new double[15];
+        double media[] = new double[15];
+        String situacao[] = new String[15];
+        
+
+        for(int i=0; i<15; i++){
+            System.out.println("Digite o nome do " + (i+1) + "º aluno:  ");
+            nome[i] = recebe.nextLine();
+
+            System.out.println("Digite a primeira nota do " + (i+1) + "º aluno:  ");
+            notaPr1[i] = recebe.nextDouble();
+
+            System.out.println("Digite a primeira nota do " + (i+1) + "º aluno:  ");
+            notaPr2[i] = recebe.nextDouble();
+
+            recebe.nextLine();
+
+            media[i] = (notaPr1[i] + notaPr2[i]) / 2;
+
+            if(media[i]< 6){
+                situacao[i] = "RP";
+            }else{
+                situacao[i] = "AP";
+            }
+ 
+        }
+
+    System.out.println("\nNome\tNota1\tNota2\tMédia\tSituação");
+
+        for(int i=0; i<15; i++){
+            System.out.print(nome[i] + "\t");
+            System.out.print(notaPr1[i] + "\t");
+            System.out.print(notaPr2[i] + "\t");
+            System.out.print(media[i] + "\t");
+            System.out.println(situacao[i]);
+        }
+        
+    recebe.close();
+
+    }
+}
